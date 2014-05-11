6fa2392179738f0fa30e1d50ca022c03ed627388
“Bogdan” <dbogdan@gmail.com>
2014-05-11 19:47:15 +0300

    added excercise2 script

---
+++ exercise2/submissions/dbogdan.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+#
+# Author: Bogdan Vasile Dumtirica
+# Elance: http://dbogdan.elance.com
+#
+
+from scipy.stats import percentileofscore
+import pandas as pd
+
+in_file = "../input/mean.csv"
+out_file = "pct.csv"
+
+def get_percentile_from_rating(rating_list):
+    percentile_list = []
+    for r in rating_list:
+        percentile = percentileofscore(rating_list, r, kind='mean')
+        percentile_list.append(percentile)
+    return percentile_list
+
+def main():
+    mean = pd.read_csv(in_file)
+    percentile_cols = []
+    for col_name in mean:
+        if col_name == 'fdntext':
+            percentile_list = list(mean[col_name])
+        else:
+            percentile_list = get_percentile_from_rating(mean[col_name])
+        percentile_cols.append((col_name,percentile_list))
+
+        data = pd.DataFrame.from_items(percentile_cols)
+        data.to_csv(out_file, index=False)
+
+if __name__ == "__main__":
+    main()
